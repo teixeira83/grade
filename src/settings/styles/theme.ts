413cdb26c43f2bd8afe680246efac93f3970bc0d
teixeira83 <rafael.amtx@gmail.com>
2022-08-09 19:43:10 -0300

    style: add detaills color

--- src/settings/styles/theme.ts
+++ src/settings/styles/theme.ts
@@ -4,17 +4,18 @@ const theme: IStyleGuide = {
   colors: {
     primary: '#171d2b',
     secondary: '#95B8D1',
-    background: '#23CE6B',
-    text: '#fff',
+    background: '#B8C5D6',
+    details: '#FFFFFF',
+    text: '#FFFFFF',
     textLight: '#FFFFFF',
     attention: '#C83737',
     warning: '#C9AF2A',
   },
 
   fonts: {
-    small: 12,
-    medium: 14,
-    large: 16,
+    small: 14,
+    medium: 16,
+    large: 18,
   },
 
   spacing: 20,
